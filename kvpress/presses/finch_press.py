f8034e5bbb586f40ccc1b8f9673f304b216fd4b5
giulio98 <corallo.giulio@yahoo.it>
2025-04-14 13:00:59 +0200

    use SnapKV window attention for finch
    Co-authored-by: SimJeg <simjeg@users.noreply.github.com>
    
    Signed-off-by: giulio98 <corallo.giulio@yahoo.it>

--- kvpress/presses/finch_press.py
+++ kvpress/presses/finch_press.py
@@ -36,43 +36,6 @@ class FinchPress(BasePress):
     normalize_scores: bool = True
     condition_len: int = None  # calculate on length of question dynamically
 
-    def compute_finch_attention(self, module, hidden_states, keys, condition_len, position_embeddings, kwargs):
-        """
-        Compute the last `condition_len` queries (question) and associated attention weights
-        for the first `q_len - condition_len` keys (context).
-        """
-        bsz, q_len, _ = hidden_states.shape
-        num_heads = module.config.num_attention_heads
-        head_dim = module.head_dim
-        num_key_value_groups = num_heads // module.config.num_key_value_heads
-
-        # Get question queries
-        if hasattr(module, "q_proj"):
-            query_states = module.q_proj(hidden_states[:, -condition_len:])
-        elif hasattr(module, "qkv_proj"):
-            qkv = module.qkv_proj(hidden_states[:, -condition_len:])
-            query_states = qkv[..., : num_heads * head_dim]
-        else:
-            raise NotImplementedError(f"Finch not yet implemented for {module.__class__}.")
-
-        query_states = query_states.view(bsz, condition_len, num_heads, head_dim).transpose(1, 2)
-
-        # Apply RoPE, considering queries only
-        cos, sin = position_embeddings
-        cos, sin = cos[:, -condition_len:], sin[:, -condition_len:]
-        query_states = (query_states * cos.unsqueeze(1)) + (rotate_half(query_states) * sin.unsqueeze(1))
-
-        # Compute attention for context tokens
-        key_states = repeat_kv(keys, num_key_value_groups)
-
-        attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(head_dim)
-        attention_mask = torch.ones_like(attn_weights) * float("-inf")
-        attention_mask = torch.triu(attention_mask, diagonal=key_states.shape[-2] - condition_len + 1)
-        attn_weights += attention_mask
-        attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
-        attn_weights = attn_weights[..., :-condition_len]
-        return attn_weights
-
     def score(self, module, hidden_states, keys, values, attentions, kwargs):
 
         bsz, num_key_value_heads, q_len, _ = keys.shape
@@ -81,9 +44,7 @@ class FinchPress(BasePress):
         if attentions is not None:
             attn_weights = attentions[..., -self.condition_len:, : -self.condition_len]
         else:
-            attn_weights = self.compute_finch_attention(
-                module, hidden_states, keys, self.condition_len, kwargs["position_embeddings"], kwargs
-            )
+            attn_weights = SnapKVPress.compute_window_attention(module, hidden_states, keys, self.condition_len, kwargs["position_embeddings"])
         if self.normalize_scores:
             non_zero_counts = torch.arange(q_len - self.condition_len, q_len)[None, None, :, None]
             non_zero_counts = non_zero_counts.to(attn_weights.device)
